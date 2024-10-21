e95d801e11d9a00271a6162167744e84349926eb
Rizki Pm <udarizki2020@gmail.com>
2024-10-21 11:19:33 +0800

    update

--- app/src/main/java/com/rizki/klinikapp_mi2c/MainActivity.kt
+++ app/src/main/java/com/rizki/klinikapp_mi2c/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.rizki.klinikapp_mi2c.screen.HomeDoctorsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         //splash screen
         //handle untuk delay selama beberapa detik
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, StartScreen2::class.java)
+            val intent = Intent(this, HomeDoctorsActivity::class.java)
             startActivity(intent)
             finish() //agar ketika dia pindah ke page ke 2, itu gak back ke splash screen
         }, 3000)
--- app/src/main/java/com/rizki/klinikapp_mi2c/screen/HomeDoctorsActivity.kt
+++ app/src/main/java/com/rizki/klinikapp_mi2c/screen/HomeDoctorsActivity.kt
@@ -5,17 +5,85 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.rizki.klinikapp_mi2c.R
+import com.rizki.klinikapp_mi2c.adapter.ListDoctorsAdapter
+import com.rizki.klinikapp_mi2c.model.ModelListDoctors
 
 class HomeDoctorsActivity : AppCompatActivity() {
+
+    private  var rv_list_doctors: RecyclerView? = null
+    private var homeDoctorsAdapter : ListDoctorsAdapter? = null
+
+    //array data doctors
+    private var doctorList = mutableListOf<ModelListDoctors>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_doctors)
+
+        rv_list_doctors = findViewById(R.id.rv_list_doctors)
+        doctorList = ArrayList()
+        homeDoctorsAdapter = ListDoctorsAdapter(doctorList);
+        val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(this,1)
+        rv_list_doctors!!.layoutManager = layoutManager
+        rv_list_doctors!!.adapter = homeDoctorsAdapter
+
+        //create dummy data
+        prepareDataDoctors()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    private fun prepareDataDoctors(){
+        var itemDoctors = ModelListDoctors(
+            R.drawable.dokter1,
+            "Rizki Syaputra",
+            "Dentist",
+            "120",
+            "5.0"
+        )
+        doctorList.add(itemDoctors)
+        itemDoctors = ModelListDoctors(
+            R.drawable.dokter6,
+            "Yahya Badrusssalam Ar Razki",
+            "Obgyn",
+            "100",
+            "5.0"
+        )
+        doctorList.add(itemDoctors)
+        itemDoctors = ModelListDoctors(
+            R.drawable.dokter3,
+            "Dr. Abdullan",
+            "Obgyn",
+            "100",
+            "5.0"
+        )
+        doctorList.add(itemDoctors)
+        itemDoctors = ModelListDoctors(
+            R.drawable.dokter4,
+            "Drs Rini Mardan",
+            "Obgyn",
+            "100",
+            "5.0"
+        )
+        doctorList.add(itemDoctors)
+        itemDoctors = ModelListDoctors(
+            R.drawable.dokter5,
+            "Prof. Abdul Samad",
+            "Dokter Umum",
+            "100",
+            "5.0"
+        )
+        doctorList.add(itemDoctors)
+
+        homeDoctorsAdapter!!.notifyDataSetChanged()
+    }
+
 }
